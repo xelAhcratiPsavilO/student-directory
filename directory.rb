b6577988d3a0cf9f2d91e28b69f06b5ee8c65815
Alex <alejandropitarcholivas@gmail.om>
2018-02-16 13:36:34 +0000

    Add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,13 +1,17 @@
 def input_students
 	puts "\nPlease enter the names of the students"
-	puts "\nTo finish, just hit return twice"
+	puts "\nTo finish, just hit return"
 	# create an empty array
 	students = []
 	# get the first name
-	name = gets
+	name = gets.chomp
+	if name == ""
+		puts "There are no students, end of the class!\n\n"
+		return
+  end
 	# while the name is not empty, repeat this code
 	while !name.empty? do
-	  puts "What cohort are you part of?".gsub(/[W]/, "\nW")
+	  puts "What cohort are you part of?"
 	  month = gets.chomp.upcase
 		  if month == ""
 				month = "FEBRUARY"
@@ -21,12 +25,7 @@ def input_students
 		#add the student hash to the array
 		students << {name: name, cohort: month, hobbies: hobby, origin: country, height: tall}
 		if students.count == 1
-			if name == "\n"
-				puts "There are no students, end of the class!"
-				exit
-			else
 			  puts "Now we have 1 student"
-			end
 		else
 		  puts "Now we have #{students.count} students"
 		end
@@ -43,6 +42,10 @@ def print_header
 end
 
 def print(students)
+  if students == nil
+	  puts "There are no students!\n\n"
+	  return
+  end
 	puts "What letter are you looking for?"
 	letter = gets.chomp
 	i = 0
@@ -55,16 +58,22 @@ def print(students)
 end
 
 def print_footer(names)
-	 if names.count == 1
-		 puts "Overall, we have 1 great student"
-	 else
-     puts "Overall, we have #{names.count} great students"
-	 end
+  if names == nil
+	  return
+  end
+  if names.count == 0
+    puts "There are no students!\n\n"
+	elsif names.count == 1
+	  puts "Overall, we have 1 great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
-students = input_students
-
 def cohort(students)
+  if students == nil
+	  return
+	end
   cohort = {}
   students.each do |x|
     cohorts = x[:cohort]
@@ -79,7 +88,36 @@ def cohort(students)
 end
 
 #nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
-cohort(students)
+#remove '#' to students = input_students
+#remove '#' to print_header
+#remove '#' to print(students)
+#remove '#' to print_footer(students)
+#remove '#' to cohort(students)
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+			cohort(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
