8bc03536eb3588dc51c2406688763a7ae5d70e9b
Alex <alejandropitarcholivas@gmail.om>
2018-02-16 10:22:08 +0000

    Add adjustment to 1 student

--- directory.rb
+++ directory.rb
@@ -1,33 +1,33 @@
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	puts "\nPlease enter the names of the students"
+	puts "\nTo finish, just hit return twice"
 	# create an empty array
 	students = []
 	# get the first name
 	name = gets.chomp
 	# while the name is not empty, repeat this code
-
 	while !name.empty? do
 	  puts "What cohort are you part of?"
 	  month = gets.chomp.upcase
-
 		  if month == ""
 				month = "FEBRUARY"
 			end
-
 		puts "What is your hobby?"
 		hobby = gets.chomp
 	  puts "Where do you come from?"
 	  country = gets.chomp
-	  puts "How tall are you?"
+	  puts "How tall are you? (in cm)"
 	  tall = gets.chomp
 		#add the student hash to the array
 		students << {name: name, cohort: month, hobbies: hobby, origin: country, height: tall}
-		puts "Now we have #{students.count} students"
+		if students.count == 1
+			puts "Now we have 1 student"
+		else
+		  puts "Now we have #{students.count} students"
+		end
 		# get another name from the user
 		name = gets.chomp
 	end
-
 	# return the array of input_students
 	students
 end
@@ -41,39 +41,36 @@ def print(students)
 	puts "What letter are you looking for?"
 	letter = gets.chomp
 	i = 0
-
 	while i < students.length do
-
 		if "#{students[i][:name]}".start_with?(letter.upcase) and "#{students[i][:name]}".length < 12
-  	  puts "#{(i + 1)}. #{students[i][:name]} (#{students[i][:cohort]} cohort) who loves #{students[i][:hobbies]} and comes from #{students[i][:origin]}. #{students[i][:name]} is #{students[i][:height]}cm tall.".center(150,"-")
+  	  puts "#{(i + 1)}. #{students[i][:name]} loves #{students[i][:hobbies]} and comes from #{students[i][:origin]}. #{students[i][:name]} is #{students[i][:height]}cm tall.".center(150,"-")
 		end
-
 		i += 1
   end
-
 end
-
  def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+	 if names.count == 1
+		 puts "Overall, we have 1 great student"
+	 else
+     puts "Overall, we have #{names.count} great students"
+	 end
 end
-#nothing happens until we call the methods
+
 students = input_students
+
+#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
 
 cohort = {}
-
 students.each do |x|
   cohorts = x[:cohort]
   person = x[:name]
-
   if cohort[cohorts] == nil
     cohort[cohorts] = [person]
   else
     cohort[cohorts].push(person)
   end
-
 end
-
 puts cohort.to_a
