c7875d22d9b60b84dabfe91152480dfab66fb676
Alex <alejandropitarcholivas@gmail.om>
2018-02-16 16:46:39 +0000

    Add retrieve info from file

--- directory.rb
+++ directory.rb
@@ -80,6 +80,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 def show_students
@@ -99,6 +100,14 @@ def save_students
   end
   file.close
 end
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 def process(selection)
   case selection
     when "1"
@@ -107,6 +116,8 @@ def process(selection)
       show_students
 		when "3"
       save_students
+		when "4"
+      load_students
     when "9"
       exit
     else
